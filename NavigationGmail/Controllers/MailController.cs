8383186061e372f765f6d0b196489586d8c2cb34
Graham Mendick <graham.mendick@gmail.com>
2014-10-13 21:26:57 +0100

    Add Controller methods

--- NavigationGmail/Controllers/MailController.cs
+++ NavigationGmail/Controllers/MailController.cs
@@ -1,18 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Navigation;
+using NavigationGmail.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace NavigationGmail.Controllers
 {
     public class MailController : Controller
     {
-        //
-        // GET: /Mail/
-        public ActionResult Index()
-        {
-            return View();
-        }
+		private MailRepository _Repository = new MailRepository();
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[ChildActionOnly]
+		public ActionResult _Content(string folder, int? id, int start)
+		{
+			var model = new MailViewModel();
+			if (!id.HasValue)
+				ShowList(model, folder, start);
+			else
+				ShowDetails(model, id.Value);
+			return PartialView(model);
+		}
+
+		private void ShowList(MailViewModel model, string folder, int start)
+		{
+			ViewBag.Title = folder == "inbox" ? "Inbox" : "Sent Mail";
+			model.Conversations = _Repository.Conversations.Where(c => c.Folder == folder);
+			StateContext.Data.Clear();
+			StateContext.Bag.folder = folder;
+			StateContext.Bag.start = start;
+			model.Count = model.Conversations.Count();
+			model.Conversations = model.Conversations.Skip(start).Take(50);
+		}
+
+		private void ShowDetails(MailViewModel model, int id)
+		{
+			ViewBag.Title = model.Conversation.Description;
+			model.Conversation = model.Conversations.FirstOrDefault(c => c.Id == id);
+			var latest = model.Conversation.Messages.Last();
+			StateContext.Data["id" + latest.Id] = "o";
+			foreach (var mail in model.Conversation.Messages)
+			{
+				mail.Open = StateContext.Data["id" + mail.Id] != null;
+				mail.Latest = mail == latest;
+			}
+		}
+
+		public ActionResult Send(int id, Message message)
+		{
+			_Repository.AddMessage(id, message.Text);
+			ModelState.Remove("mail.Message");
+			StateContext.Bag.sent = true;
+			return View();
+		}
 	}
 }

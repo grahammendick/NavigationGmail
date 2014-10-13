3759bf348a6ac6d5f8b17eb6fee7a0e95bdcfa47
Graham Mendick <graham.mendick@gmail.com>
2014-10-13 20:31:56 +0100

    Added classes and views

---
+++ NavigationGmail/Controllers/MailController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NavigationGmail.Controllers
+{
+    public class MailController : Controller
+    {
+        //
+        // GET: /Mail/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
